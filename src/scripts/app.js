969c72a9fa037ef8bdc2fae36ad8ee2bfde6badb
The0d0r <moriotheodore@gmail.com>
2017-12-21 14:38:41 +0100

    Player
    add player + sound

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,7 @@ import './sections/landing.js'
 import Surrounding from './components/Surrounding'
 import Rocket from './components/Rocket'
 import Slider from './components/Slider'
+import player from './components/player'
 
 const coords = {
 	x: 28.4583995,
@@ -17,3 +18,5 @@ const littleRocket = new Rocket('littleRocket')
 const slider = new Slider('.steps', surrounding)
 slider.touchmoves()
 slider.dots()
+
+const player = new player()
---
+++ src/scripts/components/player.js
@@ -0,0 +1,16 @@
+export default class player {
+    const $music_ambiant = document.querySelector('audio')
+    const $music_btn = document.querySelector('.music_btn')
+    
+    $music_btn.addEventListener('click', ()=> {
+        if($music_ambiant.pause() == true){
+            $music_ambiant.play()
+            $music_btn.src = 'assets/images/src/sound-on.png'
+        }
+        else{
+            $music_ambiant.pause()
+            $music_btn.src = 'assets/images/src/sound-off.png'
+    
+        }
+    })
+}
